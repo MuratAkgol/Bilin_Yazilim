cec6c8a745aa3d17f2980ec1ff113bf589c530d8
MuratAkgol <akgolmurat4@gmail.com>
2023-09-18 11:09:30 +0300

    Bilin Yazılım ve Bilişim Danışmanlığı

---
+++ Bilin_Otomasyon/Hesaplar.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilin_Otomasyon
+{
+    public class Hesaplar
+    {
+        public static int UcretHesala(int GirSaat, int GirDakika, int CikSaat, int CikDakika)
+        {
+            int Ucret = 0;
+            
+            if (CikDakika < GirDakika && CikSaat > GirSaat)
+            {
+                int a = CikSaat - 1;
+                int GecenSureSaatHesabi = (a * 60) - (GirSaat * 60);
+                int GecenSureSaati = GecenSureSaatHesabi / 60;
+
+                if (GecenSureSaati <= 3)
+                {
+                    Ucret = GecenSureSaati * 3;
+                }
+                else if (GecenSureSaati >= 3 && GecenSureSaati < 6)
+                {
+                    int c = GecenSureSaati - 2;
+                    Ucret = (c * 2) + 9;
+                }
+                else
+                {
+                    int c = GecenSureSaati - 5;
+                    c = (c / 3) +1 ;
+                    Ucret = (c * 5) + 15;
+                }
+            }
+            else if (GirSaat > CikSaat)
+            {
+                int a = CikSaat;
+                int GecenSureSaatHesabi = (a * 60) - (GirSaat * 60);
+                int GecenSureSaati = GecenSureSaatHesabi / 60;
+                GecenSureSaati += 24;
+
+                if (GecenSureSaati <= 3)
+                {
+                    Ucret = GecenSureSaati * 3;
+                }
+                else if (GecenSureSaati >= 3 && GecenSureSaati < 6)
+                {
+                    int c = GecenSureSaati - 2;
+                    Ucret = (c * 2) + 9;
+                }
+                else
+                {
+                    int c = GecenSureSaati - 5;
+                     c =(c / 3) +1;
+                    Ucret = (c * 5) + 15;
+                }
+
+                
+            }
+            else
+            {
+                int GecenSureSaatHesabi = (CikSaat) - (GirSaat);
+                int b = (CikDakika - GirDakika);
+
+                if (b < 0)
+                {
+                    b = b + 60;
+                }
+
+                if (GecenSureSaatHesabi == 0)
+                {
+                    Ucret = 3;
+                }
+                else if (GecenSureSaatHesabi <= 3)
+                {
+
+                    Ucret = GecenSureSaatHesabi * 3;
+                }
+                else if (GecenSureSaatHesabi >= 3 && GecenSureSaatHesabi < 6)
+                {
+                    int a = GecenSureSaatHesabi - 2;
+                    Ucret = (a * 2) + 9;
+                }
+                else
+                {
+                    int a = GecenSureSaatHesabi - 5;
+                     a =(a / 3) +1 ;
+
+                    Ucret = (a * 5) + 15;
+                }
+            }
+            return Ucret;
+        }
+
+        public static string SaatHesapla(int GirSaat, int GirDakika, int CikSaat, int CikDakika)
+        {
+            string DakikaHesabi = "";
+            if (CikDakika < GirDakika && CikSaat > GirSaat)
+            {
+                int saat = CikSaat - 1;
+                int GecenSureSaatHesabi = (saat * 60) - (GirSaat * 60);
+                int GecenSureSaati = GecenSureSaatHesabi / 60;
+                int b = (CikDakika - GirDakika) + 60;
+                DakikaHesabi = "Geçirilen süre: " + GecenSureSaati + " saat " + b + " dakika. ";
+            }
+            else if (GirSaat > CikSaat)
+            {
+                int saat = CikSaat;
+                int GecenSureSaatHesabi = (saat * 60) - (GirSaat * 60);
+                int GecenSureSaati = GecenSureSaatHesabi / 60;
+                GecenSureSaati += 24;
+                DakikaHesabi = "Geçirilen süre: " + GecenSureSaati + " saat " + saat + " dakika. ";
+            }
+            else
+            {
+                int GecenSureSaatHesabi = (CikSaat) - (GirSaat);
+                int b = (CikDakika - GirDakika);
+
+                if (b < 0)
+                {
+                    b = b + 60;
+                }
+                DakikaHesabi = "Geçirilen süre: " + GecenSureSaatHesabi + " saat " + b + " dakika. ";
+            }
+
+            return DakikaHesabi;
+        }
+
+        public static bool Format(string GirSaat, string CikSaat)
+        {
+            bool Format = false;
+
+            if (GirSaat.Length != 5 || CikSaat.Length != 5 || !GirSaat.Contains(":") || !CikSaat.Contains(":") )
+            {
+                Format = true;
+            }
+
+            return Format;
+        }
+    }
+}
---
+++ Bilin_Otomasyon/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilin_Otomasyon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool devamEt = true;
+
+            while (devamEt)
+            {
+                Console.WriteLine("Aracın giriş saatini giriniz:");
+                string Giris_saat = Console.ReadLine();
+                Console.WriteLine("Aracın çıkış saatini giriniz:");
+                string Cikis_saat = Console.ReadLine();
+                
+                string[] Giris_ayir = Giris_saat.Split(':');
+                string[] Cikis_ayir = Cikis_saat.Split(':');
+
+                bool Format = Hesaplar.Format(Giris_saat,Cikis_saat);
+
+                if (Format)
+                {
+                    Console.WriteLine("Saati istenen formatta girmediniz. (istenilen format= 07:15)");
+                    continue;
+                }
+
+                var UcretHesaplama = Hesaplar.UcretHesala(Convert.ToInt32(Giris_ayir[0]), Convert.ToInt32(Giris_ayir[1]), Convert.ToInt32(Cikis_ayir[0]), Convert.ToInt32(Cikis_ayir[1]));
+                var SaatHesapla = Hesaplar.SaatHesapla(Convert.ToInt32(Giris_ayir[0]), Convert.ToInt32(Giris_ayir[1]), Convert.ToInt32(Cikis_ayir[0]), Convert.ToInt32(Cikis_ayir[1]));
+                Console.WriteLine(SaatHesapla + " Ücret: " + UcretHesaplama + " TL");
+
+                Console.WriteLine("Devam etmek istiyor musunuz? (Evet/Hayır)");
+                string devamCevabi = Console.ReadLine();
+
+                if (devamCevabi.ToLower() != "evet")
+                {
+                    devamEt = false;
+                }
+            }
+
+            Console.WriteLine("Program sona erdi.");
+        }
+    }
+}
+
+
